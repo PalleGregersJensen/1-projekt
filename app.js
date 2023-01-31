10618cff7b41a27db49bfdf6b35d1f09c3fc50e3
PalleGregersJensen <palle.gregers.jensen@gmail.com>
2023-01-31 12:40:20 +0100

    1. project w. changes
    Ændringer: Baggrundsfarve, Console(log), alert, m.m.

---
+++ app.js
@@ -0,0 +1,5 @@
+document.body.innerHTML += "Hello JaveScript!";
+//alert("Hello Dat.js");
+console.log("Hello Dat.js");
+console.log("Hello students!");
+console.log("This is your first JavaScript app!");
